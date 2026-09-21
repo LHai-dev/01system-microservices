ebffa79b038d3ef510788d9f25f88242fa9e1a80
Limhai <72647458+LHai-dev@users.noreply.github.com>
2026-09-21 14:20:17 +0700

    refactor : remove concrete class

--- ecommerce-system/order-service/order-service-domain/order-application-service/src/main/java/com/mptc/training/ecommerce/order/domain/usecase/CreateOrderUseCaseImpl.java
+++ ecommerce-system/order-service/order-service-domain/order-application-service/src/main/java/com/mptc/training/ecommerce/order/domain/usecase/CreateOrderUseCaseImpl.java
@@ -2,11 +2,7 @@ package com.mptc.training.ecommerce.order.domain.usecase;
 
 import com.mptc.training.ecommerce.order.domain.dto.CreateOrderCommand;
 import com.mptc.training.ecommerce.order.domain.dto.CreateOrderResult;
-import com.mptc.training.ecommerce.order.domain.exception.OrderDomainException;
 import com.mptc.training.ecommerce.order.domain.port.input.CreateOrderUseCase;
-import com.mptc.training.ecommerce.order.domain.port.output.BusinessRepository;
-import com.mptc.training.ecommerce.order.domain.port.output.CustomerRepository;
-import com.mptc.training.ecommerce.order.domain.valueobject.BusinessId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -18,19 +14,12 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CreateOrderUseCaseImpl implements CreateOrderUseCase {
 
-    private final CustomerRepository customerRepository;
-    private final BusinessRepository businessRepository;
-
-    @Override
     public CreateOrderResult execute(CreateOrderCommand createOrderCommand) {
-        log.info("create order Use Case. {}", createOrderCommand);
+        log.info("executing CreateOrderUseCase: {}", createOrderCommand);
 
-        customerRepository.findCustomer(createOrderCommand.customerId()).orElseThrow(() -> new OrderDomainException("Could not find customer with id: " + createOrderCommand.customerId()));
+        // Validate customer
 
-        BusinessId businessId = new BusinessId(createOrderCommand.businessId());
-
-        businessRepository.findBusiness(businessId).orElseThrow(() -> new OrderDomainException("Could not find business with id: " + createOrderCommand.businessId()));
-
+        // Validate business
 
         return new CreateOrderResult(UUID.randomUUID());
     }
